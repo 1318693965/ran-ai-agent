3c3d9ded4c302482625c5cf76466adcb33ef5ed6
zhr <1318693965@qq.com>
2025-06-25 21:38:16 +0800

    all

---
+++ src/main/java/com/xiaoyu/ranaiagent/RanAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.ranaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoyu/ranaiagent/RanAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.ranaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
